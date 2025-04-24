351956c1b9fc886d996f1e2c987a65e205e903d7
ij23 <ij23@corgi.doc.ic.ac.uk>
2025-04-24 20:22:22 +0100

    bandit_scripts

---
+++ scripts/bandits/run_bandits_ucb1_variance.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for ((i = $2; i <= $3; i++))
+do
+    python run_bandit_classification.py --num_random_trials 5 --num_trials 200 --bandit_exploration_rate $1 --save_directory "exploration_ucb1_$1" --bandit_seed $i --numpy_seed $i --run_name "exp_${1}_ucb1_seed_$i" --uncertainty_type ucb1
+done
