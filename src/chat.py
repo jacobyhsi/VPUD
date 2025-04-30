ff11c427a8ac5043f6f0383e0b1523c5614f0413
ij23 <ij23@corgi.doc.ic.ac.uk>
2025-04-30 20:57:02 +0100

    chat_ip_update

--- src/chat.py
+++ src/chat.py
@@ -2,8 +2,8 @@ import requests
 import math
 from openai import OpenAI
 
-def chat(message: str, label_keys, seed: int, model: str ="Qwen/Qwen2.5-14B", port: str = "8000"):
-    url = f"http://localhost:{port}/v1/completions"
+def chat(message: str, label_keys, seed: int, model: str ="Qwen/Qwen2.5-14B", port: str = "8000", ip: str = "localhost"):
+    url = f"http://{ip}:{port}/v1/completions"
     headers = {"Content-Type": "application/json"}
     data = {
         "model": model,
@@ -38,8 +38,8 @@ def chat(message: str, label_keys, seed: int, model: str ="Qwen/Qwen2.5-14B", po
 
     return text_output, normalized_probs
 
-def chat_response_only(message: str, seed: int, max_tokens: int=10, model: str="Qwen/Qwen2.5-14B", port: str="8000"):
-    url = f"http://localhost:{port}/v1/completions"
+def chat_response_only(message: str, seed: int, max_tokens: int=10, model: str="Qwen/Qwen2.5-14B", port: str="8000", ip: str="localhost"):
+    url = f"http://{ip}:{port}/v1/completions"
     headers = {"Content-Type": "application/json"}
     data = {
         "model": model,
