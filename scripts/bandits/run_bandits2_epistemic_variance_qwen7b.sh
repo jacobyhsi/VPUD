09a81f22f9c999389a56f36350d42d7d0484487f
ij23 <ij23@corgi.doc.ic.ac.uk>
2025-05-01 01:00:51 +0100

    extend_bandits

--- scripts/bandits/run_bandits2_epistemic_variance_qwen7b.sh
+++ scripts/bandits/run_bandits2_epistemic_variance_qwen7b.sh
@@ -2,5 +2,5 @@
 
 for ((i = $2; i <= $3; i++))
 do
-    python run_bandit_classification.py --num_random_trials 5 --num_trials 100 --bandit_midpoint 0.6 --bandit_exploration_rate $1 --save_directory "qwen7b/experiment_2/exploration_ev_${1}" --bandit_seed $i --numpy_seed $i --run_name "exp_${1}_ev_seed_$i" --model_name Qwen/Qwen2.5-7B --model_port 9000
+    python run_bandit_classification.py --num_random_trials 5 --num_trials 200 --bandit_midpoint $4 --bandit_exploration_rate $1 --save_directory "qwen7b/experiment_2/exploration_mid_${4}_ev_${1}" --bandit_seed $i --numpy_seed $i --run_name "exp_${1}_ev_seed_$i" --model_name Qwen/Qwen2.5-7B --model_port 9000
 done
--- scripts/bandits/run_bandits2_total_variance_llama8b.sh
+++ scripts/bandits/run_bandits2_total_variance_llama8b.sh
@@ -2,5 +2,5 @@
 
 for ((i = $2; i <= $3; i++))
 do
-    python run_bandit_classification.py --num_random_trials 5 --num_trials 100 --bandit_midpoint 0.6 --bandit_exploration_rate $1 --save_directory "llama8b/experiment_2/exploration_tv_${1}" --bandit_seed $i --numpy_seed $i --run_name "exp_${1}_tv_seed_$i" --uncertainty_type total_variance --model_name meta-llama/Meta-Llama-3-8B --model_port 7000
+    python run_bandit_classification.py --num_random_trials 5 --num_trials 200 --bandit_midpoint $4 --bandit_exploration_rate $1 --save_directory "llama8b/experiment_2/exploration_mid_${4}_tv_${1}" --bandit_seed $i --numpy_seed $i --run_name "exp_${1}_tv_seed_$i" --uncertainty_type total_variance --model_name meta-llama/Meta-Llama-3-8B --model_port 7000
 done
